f29ef515a920e574e4d45d37605e4d7eaf28610e
José Luis Salinas <sadi78@gmail.com>
2016-08-21 03:28:25 +0200

    Add Writers\Console

---
+++ src/Writers/Console.php
@@ -0,0 +1,14 @@
+<?php
+namespace JLSalinas\RWGen\Writers;
+
+use JLSalinas\RWGen\Writer;
+
+class Console extends Writer
+{
+    protected function outputGenerator()
+    {
+        while ( ($data = yield) !== null ) {
+            print_r($data);
+        }
+    }
+}
