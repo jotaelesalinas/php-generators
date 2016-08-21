c08f8b5023df22da0f5326e85e9bfd997e4585d7
José Luis Salinas <sadi78@gmail.com>
2016-08-21 02:51:54 +0200

    Undo extending Generator
    `\Generator` is marked `final` and cannot be extended :(

--- src/Writer.php
+++ src/Writer.php
@@ -8,7 +8,7 @@ namespace JLSalinas\RWGen;
  *
  * Of course, they can implement many other helper methods as needed.
  */
-abstract class Writer extends Generator implements GeneratorAggregate
+abstract class Writer implements GeneratorAggregate
 {
     use GeneratorAggregateHack;
     use WithOptions;
